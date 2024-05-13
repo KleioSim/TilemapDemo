82fe234f00686e751210d124af0665de03710c74
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-05-13 15:20:38 +0800

    refator

--- TileMapTerrain.cs
+++ TileMapTerrain.cs
@@ -1,8 +1,130 @@
 ﻿using Godot;
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class TileMapTerrain : TileMap
 {
+    internal void GenerateMap(TileMap baseMap)
+    {
+        var random = new Random();
 
+        var cellIndexs = baseMap.GetUsedCells(0);
+
+        int n = 2048 / 128;
+        foreach (var index in cellIndexs)
+        {
+            int id = baseMap.GetCellSourceId(0, index);
+            for (int x = index.X * n; x < (index.X + 1) * n; x++)
+            {
+                for (int y = index.Y * n; y < (index.Y + 1) * n; y++)
+                {
+                    SetCell(0, new Vector2I(x, y), id, new Vector2I(0, 0), 0);
+                }
+            }
+        }
+
+        var edgeIndex2Factor = GetUsedCells(0).Where(index =>
+        {
+            int id = GetCellSourceId(0, index);
+            if (id == 3)
+            {
+                return false;
+            }
+
+            var neighborDict = GetNeighborCells(index);
+            return neighborDict.Values.Any(x => GetCellSourceId(0, x) == 3);
+        }).ToDictionary(x => x, _ => 1);
+
+        var eraserCount = 0;
+        var gCount = GetUsedCells(0).Where(x => GetCellSourceId(0, x) != 3).Count();
+        int turn = 1;
+        while (eraserCount * 100 / gCount < 30)
+        {
+            var eraseIndexs = new HashSet<Vector2I>();
+            foreach (var index in edgeIndex2Factor.Keys)
+            {
+                var factor = edgeIndex2Factor[index];
+
+                if (random.Next(0, 100) <= 50 / factor)
+                {
+                    eraseIndexs.Add(index);
+                }
+            }
+
+            foreach (var index in eraseIndexs)
+            {
+                edgeIndex2Factor.Remove(index);
+                SetCell(0, index, 3, Vector2I.Zero, 0);
+                eraserCount++;
+            }
+
+            foreach (var key in edgeIndex2Factor.Keys)
+            {
+                edgeIndex2Factor[key] *= 2;
+            }
+
+            foreach (var index in eraseIndexs)
+            {
+                var neighbors = GetNeighborCells(index).Values.Where(x => GetCellSourceId(0, x) != -1 && GetCellSourceId(0, x) != 3);
+                foreach (var neighbor in neighbors)
+                {
+                    edgeIndex2Factor.TryAdd(neighbor, 1);
+                }
+            }
+
+            turn++;
+        }
+
+        var groups = new List<(HashSet<Vector2I> live, HashSet<Vector2I> ulive)>();
+
+        var landCells = new Stack<Vector2I>(GetUsedCells(0).Where(x => GetCellSourceId(0, x) != 3));
+        while (landCells.Count > 0)
+        {
+            var currIndex = landCells.Pop();
+
+            var inGroups = groups.Where(group => group.live.Any(item => GetNeighborCells(item).Values.Contains(currIndex)))
+                .OrderByDescending(x => x.live.Count + x.ulive.Count)
+                .ToArray();
+
+            if (inGroups.Length == 0)
+            {
+                groups.Add((new HashSet<Vector2I>() { currIndex }, new HashSet<Vector2I>()));
+            }
+            else
+            {
+                inGroups[0].live.Add(currIndex);
+
+                var ulivesNew = GetNeighborCells(currIndex).Values.Where(x => inGroups[0].live.Contains(x))
+                    .Append(currIndex)
+                    .Where(index => !GetNeighborCells(index).Values.Except(inGroups[0].live).Any())
+                    .ToArray();
+
+                inGroups[0].live.ExceptWith(ulivesNew);
+                inGroups[0].ulive.UnionWith(ulivesNew);
+
+                for (int i = 1; i < inGroups.Length; i++)
+                {
+                    inGroups[0].live.UnionWith(inGroups[i].live);
+                    inGroups[0].ulive.UnionWith(inGroups[i].ulive);
+                    groups.Remove(inGroups[i]);
+                }
+
+            }
+        }
+
+        var maxItemCountGroup = groups.MaxBy(x => x.ulive.Count + x.live.Count);
+        groups.Remove(maxItemCountGroup);
+
+        foreach (var index in groups.SelectMany(x => x.ulive.Concat(x.live)))
+        {
+            SetCell(0, index, 3, Vector2I.Zero, 0);
+        }
+    }
+
+    private Dictionary<TileSet.CellNeighbor, Vector2I> GetNeighborCells(Vector2I index)
+    {
+        var directs = new[] { TileSet.CellNeighbor.TopSide, TileSet.CellNeighbor.LeftSide, TileSet.CellNeighbor.BottomSide, TileSet.CellNeighbor.RightSide };
+        return directs.ToDictionary(x => x, x => GetNeighborCell(index, x));
+    }
 }
--- TilemapTest.cs
+++ TilemapTest.cs
@@ -10,87 +10,13 @@ public partial class TilemapTest : Control
     public TileMapBase BaseMap => GetNode<TileMapBase>("CanvasLayer2/TileMap2048");
     public TileMapTerrain TerrainMap => GetNode<TileMapTerrain>("CanvasLayer2/TileMap128");
 
-    private Random random = new Random();
-
     public override void _Ready()
     {
         button.Pressed += () =>
         {
             BaseMap.GenerateMap();
 
-            var cellIndexs = BaseMap.GetUsedCells(0);
-
-            int n = 2048 / 128;
-            foreach (var index in cellIndexs)
-            {
-                int id = BaseMap.GetCellSourceId(0, index);
-                for (int x = index.X * n; x < (index.X + 1) * n; x++)
-                {
-                    for (int y = index.Y * n; y < (index.Y + 1) * n; y++)
-                    {
-                        TerrainMap.SetCell(0, new Vector2I(x, y), id, new Vector2I(0, 0), 0);
-                    }
-                }
-            }
-
-            var edgeIndex2Factor = TerrainMap.GetUsedCells(0).Where(index =>
-            {
-                int id = TerrainMap.GetCellSourceId(0, index);
-                if (id == 3)
-                {
-                    return false;
-                }
-
-                var neighborDict = GetNeighborCells(index);
-                return neighborDict.Values.Any(x => TerrainMap.GetCellSourceId(0, x) == 3);
-            }).ToDictionary(x => x, _ => 1);
-
-            var eraserCount = 0;
-            var gCount = TerrainMap.GetUsedCellsById(0, 0).Count();
-            int turn = 1;
-            while (eraserCount * 100 / gCount < 50)
-            {
-                var eraseIndexs = new HashSet<Vector2I>();
-                foreach (var index in edgeIndex2Factor.Keys)
-                {
-                    var factor = edgeIndex2Factor[index];
-
-                    if (random.Next(0, 100) <= 50 / factor)
-                    {
-                        eraseIndexs.Add(index);
-                    }
-                }
-
-                foreach (var index in eraseIndexs)
-                {
-                    edgeIndex2Factor.Remove(index);
-                    TerrainMap.EraseCell(0, index);
-                    eraserCount++;
-                }
-
-                foreach (var key in edgeIndex2Factor.Keys)
-                {
-                    edgeIndex2Factor[key] *=2;
-                }
-
-                foreach (var index in eraseIndexs)
-                {
-                    var neighbors = GetNeighborCells(index).Values.Where(x => TerrainMap.GetCellSourceId(0, x) != -1 && TerrainMap.GetCellSourceId(0, x) != 3);
-                    foreach (var neighbor in neighbors)
-                    {
-                        edgeIndex2Factor.TryAdd(neighbor, 1);
-                    }
-                }
-
-                turn++;
-            }
-
+            TerrainMap.GenerateMap(BaseMap);
         };
-    }
-
-    private Dictionary<TileSet.CellNeighbor, Vector2I> GetNeighborCells(Vector2I index)
-    {
-        var directs = new[] { TileSet.CellNeighbor.TopSide, TileSet.CellNeighbor.LeftSide, TileSet.CellNeighbor.BottomSide, TileSet.CellNeighbor.RightSide };
-        return directs.ToDictionary(x => x, x => TerrainMap.GetNeighborCell(index, x));
     }
 }
