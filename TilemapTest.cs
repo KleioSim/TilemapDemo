2885e98ace6a612c03fd91931b2cd7dbf317e105
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-05-15 11:08:11 +0800

    remove no use tilemap

--- TilemapTest.cs
+++ TilemapTest.cs
@@ -8,7 +8,7 @@ public partial class TilemapTest : Control
 {
     public Button button => GetNode<Button>("CanvasLayer/Button");
     public TileMapBase BaseMap => GetNode<TileMapBase>("CanvasLayer2/TileMap2048");
-    public TileMapTerrain TerrainMap => GetNode<TileMapTerrain>("CanvasLayer2/TileMap128");
+    public TileMapTerrain TerrainMap => GetNode<TileMapTerrain>("CanvasLayer2/TileMapTerrain");
     public TileMap512 Terrain512 => GetNode<TileMap512>("CanvasLayer2/TileMap512");
     public TileMap1024 Terrain1024 => GetNode<TileMap1024>("CanvasLayer2/TileMap1024");
 
